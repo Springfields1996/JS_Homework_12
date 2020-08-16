fccad1737694b50e21a73dc96b3376830f421ba7
Bezzubov Artem <66363533+Springfields1996@users.noreply.github.com>
2020-08-16 23:11:17 +0300

    Changes in homework

--- src/notification.js
+++ src/notification.js
@@ -3,7 +3,7 @@ import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 import { defaults } from '@pnotify/core';
 
-defaults.delay = 1500;
+defaults.delay = 1800;
 
 function notification() {
   notice({
